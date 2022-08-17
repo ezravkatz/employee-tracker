cd54c9b8922900b669e7b6787c1d4d4467a2c6af
ezravkatz <ezravkatz@gmail.com>
2022-08-16 22:25:30 -0400

    added employee table

--- db/schema.sql
+++ db/schema.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS roles;
 
 CREATE TABLE department (
   id INT PRIMARY KEY,
-  name VARCHAR(30)
+  dept_name VARCHAR(30)
 );
 
 CREATE TABLE roles (
@@ -14,3 +14,11 @@ CREATE TABLE roles (
   CONSTRAINT fk_roles FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
 );
 
+CREATE TABLE employee (
+  id INT PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id INT,
+  manager_id INT
+  CONSTRAINT fk_employee FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL
+);
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,28 +1,47 @@
 
- INSERT INTO department (id, name)
+ INSERT INTO department (id, dept_name)
 VALUES
   (1, 'Furniture'),
   (2, 'Clothing'),
   (3, 'Produce'),
   (4, 'Electronics'),
   (5, 'Deli');
-  
+
   INSERT INTO roles (id, title, salary, department_id)
 VALUES
   (101, 'Salesperson', 20.000, 1),
-  (102, 'Salesperson', 20.000, 1),
-  (103, 'Cashier', 15.000, 1),
-  (114, 'Manager', 28.000, 1),
-  (201, 'Salesperson', 18.000, 2),
-  (202, 'Salesperson', 18.000, 2),
-  (203, 'Cashier', 15.000, 2),
-  (301, 'Stocker', 16.000, 3),
-  (302, 'Cashier', 15.000, 3),
-  (401, 'Technician', 35.000, 4),
-  (402, 'Salesperson', 21.000, 4),
-  (403, 'GeekTeam', 25.000, 4),
-  (404, 'Manager', 30.000, 4),
-  (405, 'Cashier', 14.500, 4),
-  (501, 'Butcher', 33.000, 5),
-  (502, 'Butcher', 35.000, 5),
-  (503, 'Cashier', 15.500, 5);
+  (101, 'Salesperson', 20.000, 1),
+  (102, 'Cashier', 15.000, 1),
+  (201, 'Manager', 28.000, 1),
+  (101, 'Salesperson', 18.000, 2),
+  (101, 'Salesperson', 18.000, 2),
+  (102, 'Cashier', 15.000, 2),
+  (103, 'Stocker', 16.000, 3),
+  (102, 'Cashier', 15.000, 3),
+  (104, 'Technician', 35.000, 4),
+  (101, 'Salesperson', 21.000, 4),
+  (105, 'GeekTeam', 25.000, 4),
+  (202, 'Manager', 30.000, 4),
+  (102, 'Cashier', 14.500, 4),
+  (106, 'Butcher', 33.000, 5),
+  (106, 'Butcher', 35.000, 5),
+  (102, 'Cashier', 15.500, 5);
+
+  INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+  VALUES
+  (0001, 'Emily', 'Rios', 101, 201),
+  (0002, 'Henri', 'Matisse', 101, 201),
+  (0003, 'Pablo', 'Picasso', 102, 201),
+  (0004, 'Amadeus', 'Mozart', 201, null),
+  (0005, 'Jazz', 'Jones', 101, 201),
+  (0006, 'Captain', 'America', 101, 201),
+  (0007, 'Debbie', 'Downer', 102, 201),
+  (0008, 'Bert', 'Ernie', 104, 202),
+  (0009, 'Salt', 'Pepa', 101, 201),
+  (0010, 'Peter', 'Griffin', 105, 202),
+  (0011, 'Mia', 'Martin', 202, null),
+  (0012, 'Sharon', 'Osborne', 102, 202),
+  (0013, 'Claire', 'Van', 106, 202),
+  (0014, 'Bianca', 'Bedendi', 106, 202),
+  (0015, 'Gabriella', 'Papadakis', 102, 202);
+
